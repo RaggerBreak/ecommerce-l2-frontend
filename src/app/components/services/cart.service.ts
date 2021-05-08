3a2cee55cfe5ad4eb438859abaa01f00e2f37a01
RaggerLtp <ragger.break@gmail.com>
2021-05-08 13:33:36 +0200

    add increment, decrement and remove items from shopping cart

--- src/app/components/services/cart.service.ts
+++ src/app/components/services/cart.service.ts
@@ -47,4 +47,24 @@ export class CartService {
     this.totalQuantity.next(totalQuantityValue);
 
   }
+
+  decrementQuantity(cartItem: CartItem): void {
+    cartItem.quantity--;
+
+    if (cartItem.quantity === 0) {
+      this.remove(cartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(cartItem: CartItem): void {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === cartItem.id);
+
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
+  }
+
 }
