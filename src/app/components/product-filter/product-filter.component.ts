54dd79122755904d805aee2447978778a4a60f4b
RaggerLtp <ragger.break@gmail.com>
2021-05-28 20:08:12 +0200

    refactor product-filter to use form, add validation

--- src/app/components/product-filter/product-filter.component.ts
+++ src/app/components/product-filter/product-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductFilter} from '../../common/product-filter';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-product-filter',
@@ -10,9 +11,36 @@ export class ProductFilterComponent implements OnInit {
 
   productFilter: ProductFilter = new ProductFilter();
 
-  constructor() { }
+  filterFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.filterFormGroup = this.formBuilder.group({
+
+      price: this.formBuilder.group({
+        minPrice: new FormControl('',
+          [Validators.min(0), Validators.pattern('^[0-9]*$')]),
+        maxPrice: new FormControl('',
+          [Validators.min(0), Validators.pattern('^[0-9]*$')])
+      })
+    });
+  }
+
+  get minPrice() {return this.filterFormGroup.get('price.minPrice'); }
+  get maxPrice() {return this.filterFormGroup.get('price.maxPrice'); }
+
+
+  onSubmit(): void {
+    if (this.filterFormGroup.invalid) {
+      this.filterFormGroup.markAllAsTouched();
+      return;
+    }
+
+    this.productFilter.minPrice = this.minPrice.value;
+    this.productFilter.maxPrice = this.maxPrice.value;
+
+    console.log(`minPrice: ${this.productFilter.minPrice}, maxPrice: ${this.productFilter.maxPrice}`);
 
   }
 
