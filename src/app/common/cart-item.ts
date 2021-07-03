8a2fa32447a1dd5353e79b20f17310ebaa1df983
RaggerLtp <ragger.break@gmail.com>
2021-07-03 14:59:44 +0200

    refactor images to get url from response

--- src/app/common/cart-item.ts
+++ src/app/common/cart-item.ts
@@ -4,6 +4,7 @@ export class CartItem {
   id: number;
   name: string;
   imageId: number;
+  imageUrl: string;
   unitPrice: number;
   quantity: number;
 
@@ -11,6 +12,7 @@ export class CartItem {
     this.id = product.id;
     this.name = product.name;
     this.imageId = product.imageId;
+    this.imageUrl = product._links.image.href;
     this.unitPrice = product.unitPrice;
 
     this.quantity = 1;
